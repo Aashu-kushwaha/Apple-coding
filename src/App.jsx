4b17a03fbe03f08408c1790a0ae746941a51f839
aakushwaha1234567@gmail.com <aakushwaha1234567@gmail.com>
2026-08-01 11:01:43 +0530

    completed section2

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
 import React from "react";
-import { RiNotification4Fill, RiPhoneFill, RiMenu3Line } from "@remixicon/react";
+import {
+  RiNotification4Fill,
+  RiPhoneFill,
+  RiMenu3Line,
+} from "@remixicon/react";
 
 const App = () => {
   return (
@@ -7,7 +11,9 @@ const App = () => {
       <nav>
         <div className="nav1">
           <img src="https://i.pinimg.com/736x/5e/67/1c/5e671cbe6457e88c59d9f82b8270749d.jpg" />
-          <h4>Apple <br /> coding school</h4>
+          <h4>
+            Apple <br /> coding school
+          </h4>
         </div>
 
         <div className="nav2">
@@ -17,21 +23,36 @@ const App = () => {
           <h4>Request Callback</h4>
           <h4>Classroom</h4>
           <h4></h4>
-          <h4><i> <RiNotification4Fill /></i></h4>
+          <h4>
+            <i>
+              {" "}
+              <RiNotification4Fill />
+            </i>
+          </h4>
           <img src="https://i.pinimg.com/736x/5e/67/1c/5e671cbe6457e88c59d9f82b8270749d.jpg" />
-
         </div>
 
         <div className="nav3">
-          <h4> <RiPhoneFill /></h4>
-          <h4> <RiNotification4Fill /></h4>
-          <h4><RiMenu3Line /></h4>
+          <h4>
+            {" "}
+            <RiPhoneFill />
+          </h4>
+          <h4>
+            {" "}
+            <RiNotification4Fill />
+          </h4>
+          <h4>
+            <RiMenu3Line />
+          </h4>
         </div>
       </nav>
       <main>
         <section className="section1">
           <div className="left">
-            <h1>2.0 Job Ready AI Powered Cohort: Complete Web Development + DSA + Gen-AI + Aptitude</h1>
+            <h1>
+              2.0 Job Ready AI Powered Cohort: Complete Web Development + DSA +
+              Gen-AI + Aptitude
+            </h1>
 
             <div className="tags">
               <h4>MERN Stack</h4>
@@ -41,30 +62,52 @@ const App = () => {
               <h4>Aptitude & Reasoning</h4>
             </div>
 
-            <h3>Price <span>₹</span>5999<span>₹ 11999</span>(+GST)</h3>
-            <h5>Scratch card in the End!</h5>
-            <div>
-              <button>Buy Now</button>
-              <button>View Syllabus</button>
+            <h3 className="price">
+              Price <span className="green">₹ 5999</span>
+              <span className="gray"> ₹ 11999</span>(+GST)
+            </h3>
+            <h5 className="green">Scratch card in the End!</h5>
+            <div className="buttons">
+            <button>Buy Now</button>
+            <button>View Syllabus</button>
             </div>
-            <h6>Batch Starts on <span>15th September</span></h6>
+            <h6>
+              Batch Starts on <span className="green">15th September</span>
+            </h6>
           </div>
           <div className="right">
-            <div>
-              <div>
-                <img src="https://dfdx9u0psdezh.cloudfront.net/courses/f0d09d530eb99c5e5e1b7b19.webp" alt="" />
+            <div className="rightin">
+              <div className="image">
+                <img
+                  src="https://dfdx9u0psdezh.cloudfront.net/courses/f0d09d530eb99c5e5e1b7b19.webp"
+                  alt=""
+                />
               </div>
-              <div>
-                <h4>Language <span>Hinglish</span></h4>
-                <h4>Certification <span>Yes</span></h4>
-                <h4>Schedule <span>Mon-Sat</span>(8:30 PM)</h4>
-                <h4>Total content <span>250+</span></h4>
+              <div className="image-bottom">
+                <h4>
+                  Language:&nbsp;<span className="green"> Hinglish</span>
+                </h4>
+                <h4>
+                  Certificate:&nbsp; <span className="green"> Yes</span>
+                </h4>
+                <h4>
+                  Schedule: &nbsp;<span className="green"> Mon-Sat</span>(8:30 PM)
+                </h4>
+                <h4>
+                  Total content:&nbsp; <span className="green">250+ Hours</span>
+                </h4>
               </div>
             </div>
           </div>
         </section>
       </main>
+
+      <section className="section2">
+        <h1 className="bloom">Get Placed.</h1>
+        <h1>Get Placed.</h1>
+        <h3>with job ready AI powered cohort</h3>
+      </section>
     </>
-  )
-}
-export default App
+  );
+};
+export default App;
